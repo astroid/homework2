3342fe1d5d29df20eacbb468f5577eeb4251b7bc
astroid <mehdibsh@gmail.com>
2015-03-23 09:55:08 +0100

    homework

--- JoptionInput1.java
+++ JoptionInput1.java
@@ -4,24 +4,21 @@ package homework2;
 import javax.swing.JOptionPane;
 
 public class JoptionInput1 {
-
-
+int integerNumber ;
 
 public static void main(String[] args) {
-int sum = 0;
+
 
 
 int option = JOptionPane.YES_OPTION;
 while (option == JOptionPane.YES_OPTION) {
 
-String dataString = JOptionPane.showInputDialog("Enter an int value: ");
+String dataString = JOptionPane.showInputDialog("Enter an intger value: ");
 int data = Integer.parseInt(dataString);
 
 
-
-option = JOptionPane.showConfirmDialog(null, "Continue?");
 }
 
-JOptionPane.showMessageDialog(null, "" + sum);
+JOptionPane.showMessageDialog(null, "" + );
 }
 }
